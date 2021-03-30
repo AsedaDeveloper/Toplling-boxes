52b83636bd68c87c85dfa96ed784bd0d2e40c9bf
AsedaDeveloper <74008800+AsedaDeveloper@users.noreply.github.com>
2021-03-30 12:07:45 +0000

    Add files via upload

---
+++ TopplingBoxes-master/box.js
@@ -0,0 +1,25 @@
+class Box {
+  constructor(x, y, width, height,colour) {
+    var options = {
+        'restitution':0.8,
+        'friction':0.3,
+        'density':1.0
+    }
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    this.colour = colour
+    World.add(world, this.body);
+  }
+  display(){
+    var pos =this.body.position;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    rectMode(CENTER);
+    fill(this.colour);
+    rect(0, 0, this.width, this.height);
+    pop();
+  }
+};
---
+++ TopplingBoxes-master/sketch.js
@@ -0,0 +1,31 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var box1;
+
+function setup(){
+    var canvas = createCanvas(400,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    box1 = new Box(200,300,50,50,"red");
+    box2 = new Box(240,100,50,100,"green");
+    box3 = new Box(260,50,50,40,"orange")
+    box4 = new Box(230,70,50,20,"purple")
+    ground = new Ground(200,height,400,20)
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+    console.log(box2.body.position.x);
+    console.log(box2.body.position.y);
+    console.log(box2.body.angle);
+    box1.display();
+    box2.display();
+    box3.display();
+    box4.display();
+    ground.display();
+}
